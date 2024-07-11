b4060187d6394b4d1e1457c0ea1c337f585e7608
mattepuffo <matteo.ferrone@hotmail.it>
2024-07-11 09:09:23 +0200

    getroute

---
+++ api/controllers/index/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(~0);
+ini_set('display_errors', 1);
+
+use helpers\CheckProtectedAttribute;
+
+class IndexController {
+
+  public function __construct() {}
+
+  #[CheckProtectedAttribute('false')]
+  public function get($args): false|string {
+    return json_encode(array('Versione framework' => getenv('FRAMEWORK_VERSION')));
+  }
+
+}
